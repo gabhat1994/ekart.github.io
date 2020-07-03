7248adabab993b7c5e0fb0e17cc7fc18f7e981b0
stamic1 <bhatgaurav96@gmail.com>
2020-07-03 18:09:13 +0530

    Updates

---
+++ precache-manifest.58a36d534b28c67fc19be8630ec32204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95a98b37d5236404c38452ddf448d6d",
+    "url": "/ekart.github.io/index.html"
+  },
+  {
+    "revision": "42c9b2e99e2bd2b76d8c",
+    "url": "/ekart.github.io/static/css/main.fcea02bd.chunk.css"
+  },
+  {
+    "revision": "ab3a776c0605df078d87",
+    "url": "/ekart.github.io/static/js/2.b589bf19.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/ekart.github.io/static/js/2.b589bf19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c9b2e99e2bd2b76d8c",
+    "url": "/ekart.github.io/static/js/main.44f8d8a1.chunk.js"
+  },
+  {
+    "revision": "92fb9866dbc42ae8ee43",
+    "url": "/ekart.github.io/static/js/runtime-main.62a51c53.js"
+  }
+]);
